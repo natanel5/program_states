b304c94735aa19f3323bd0d9210db8eff90daba8
Natanel <natanel.reu@gmail.com>
2024-12-23 20:26:26 +0200

    the templet file

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello world")
+import psutil
+
+
+def get_cpu_usage():
+    pass
+
+
+def get_memory_usage():
+    pass
+
+
+def get_disk_usage():
+    pass
+
+
+def get_processes_usage():
+    pass
