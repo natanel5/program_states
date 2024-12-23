fb27631c51917549ed76028d1ca0b5bd78143ce7
Natanel <natanel.reu@gmail.com>
2024-12-23 20:44:29 +0200

    Update main_logic.py

---
+++ main_logic.py
@@ -0,0 +1,9 @@
+import time
+from main import get_cpu_usage, get_memory_usage, get_disk_usage, get_processes_usage
+from alarts import check_alert
+from display import show_data
+
+def main():
+
+if __name__ == "__main__":
+    main()
